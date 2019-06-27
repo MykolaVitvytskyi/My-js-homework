87e65ca37a0650d520a25dad75611995887ae870
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-06-27 12:53:21 +0300

    redid with log. ops.

--- task 4/script.js
+++ task 4/script.js
@@ -1,13 +1,13 @@
 var yourAge = prompt('Сколько вам лет?');
 var smoking = confirm('Ты куришь?');
 
-if(yourAge < 18 && smoking == false){
+if(yourAge < 18 && !smoking){
     alert('Так держать!');
 }
-else if(yourAge < 18 && smoking == true){
+else if(yourAge < 18 && smoking){
     alert('Маме расскажу');
 }
-else if(yourAge >= 18 && smoking == false){
+else if(yourAge >= 18 && !smoking){
     alert('Молодец, и не надо');
 }
 else{
