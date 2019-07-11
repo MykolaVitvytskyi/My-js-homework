ad508a048472958f4fe93136b789bcf85f3aa831
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-07-11 16:17:33 +0300

    redid

--- task 14/script.js
+++ task 14/script.js
@@ -1,46 +1,80 @@
 var arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-var pos = 0;
-var sumPos = 0;
-var min = null;
-var max = null;
-var neg = 0;
-var even = 0;
-var odd = 0;
-var sumEven = 0;
-var sumOdd = 0;
-var equalsTo_4 = 0;
-for(i = 0; i < arr.length; i++){ 
-    if(arr[i] > 0){
-        pos++;
-        sumPos += arr[i]; 
-    }
-    if(arr[i] < min){
-        min = arr[i];
-    }
-    if(arr[i] > max){
-        max = arr[i];
-    }
-    if(arr[i] < 0){
-        neg++;
-    }
-    if(arr[i] > 0 && arr[i] % 2 !== 0){
-        odd++;
-        sumOdd += arr[i];
-    }
-    if(arr[i] > 0 && arr[i] % 2 === 0){
-        even++;
-        sumEven += arr[i];
-    }
-    if(arr[i] === 4){
-        equalsTo_4++;
-    }
+
+function checkPositive(arr){
+    var pos = 0;
+    var sumPos = 0;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] > 0){
+            pos++;
+            sumPos += arr[i]; 
+        }
+    }    
+    console.log('Всего положительных чисел = '+ pos +', а их сумма = '+ sumPos);
 }
-console.log('Всего положительных чисел = '+ pos +', а их сумма = '+ sumPos);  
-console.log('Минимальный элемент массива = '+ min);
-console.log('Максимальный элемент массива = '+ max);
-console.log('Всего отрицательных чисел = '+ neg);
-console.log('Всего положительных четных чисел = '+ even);  
-console.log('Всего положительных нечетных чисел = '+ odd);
-console.log('Cумма положительных четных чисел = '+ sumEven);
-console.log('Сумма положительных нечетных чисел = '+ sumOdd);
-console.log('Количество элементов равных "4"  = '+ equalsTo_4);
+function checkMin(arr){
+    var min = null;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }    
+    console.log('Минимальный элемент массива = '+ min);
+}
+function checkMax(arr){
+    var max = null;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }    
+    console.log('Максимальный элемент массива = '+ max);
+}
+function checkNegative(arr){
+    var neg = 0;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] < 0){
+            neg++;
+        }
+    }    
+    console.log('Всего отрицательных чисел = '+ neg);
+}
+function checkEven(arr){
+    var even = 0;
+    var sumEven = 0;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] > 0 && arr[i] % 2 === 0){
+            even++;
+            sumEven += arr[i];
+        }
+    }    
+    console.log('Всего положительных четных чисел = '+ even);
+    console.log('Cумма положительных четных чисел = '+ sumEven);
+}
+function checkOdd(arr){
+    var odd = 0;
+    var sumOdd = 0;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] > 0 && arr[i] % 2 !== 0){
+            odd++;
+            sumOdd += arr[i];
+        }
+    }
+    console.log('Всего положительных нечетных чисел = '+ odd);
+    console.log('Сумма положительных нечетных чисел = '+ sumOdd);
+}
+function checkEqualsTo_4(arr){
+    var equalsTo_4 = 0;
+    for(i = 0; i < arr.length; i++){ 
+        if(arr[i] === 4){
+            equalsTo_4++;
+        }
+    }
+    console.log('Количество элементов равных "4"  = '+ equalsTo_4);
+}
+checkPositive(arr);
+checkMin(arr);
+checkMax(arr);
+checkNegative(arr);
+checkEven(arr);
+checkOdd(arr);
+checkEqualsTo_4(arr);
