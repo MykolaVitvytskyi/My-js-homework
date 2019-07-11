9d84c623cfeac13ca449219dde591e509edfc745
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-07-11 16:26:44 +0300

    redid

--- task 15/script.js
+++ task 15/script.js
@@ -1,13 +1,14 @@
 var number = prompt('Введите число');
-var even = 0;
 
 function getNumberOfEven(n){
+    var even = 0;
     for (var i = 0; i < number.length; i++){ 
         if (number[i] % 2 === 0){ 
         even++;
         }
     }
+    return even
 }
 
-getNumberOfEven(number);
+var even = getNumberOfEven(number);
 alert(even);
