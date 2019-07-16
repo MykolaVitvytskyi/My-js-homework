1ad7849b468fdf162b8fbe115ad730ee49804d10
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-07-17 02:15:47 +0300

    redid

--- task 16/script.js
+++ task 16/script.js
@@ -2,14 +2,12 @@ var nums1 = [0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1];
 var nums2 = [0,0,1,1,1,1,0,1,1,1];
 
 function arrCompress(arr){
-    var count = 1;
-    var result = [];
-    for(i = 0; i < arr.length; i++){
+    var result = [1];
+    for(i = 0; i < arr.length-1; i++){
         if(arr[i] === arr[i+1]){
-            count++;
-        }else{
-            result.push(count);
-            count = 1;
+            result[result.length-1]++;
+        }else{  
+            result.push(1);
         }
     }
     return result;
