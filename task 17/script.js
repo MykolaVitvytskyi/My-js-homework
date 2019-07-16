69cf86c2169c069ac46b66afefcd343c0b138e47
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-07-17 02:19:47 +0300

    redid

--- task 17/script.js
+++ task 17/script.js
@@ -1,17 +1,17 @@
-var person_1 = {name: 'Nikolay', age: 18, gender: 'male' };
-var person_2 = {name: 'Alexander', age: 25, gender: 'male' };
-var person_3 = {name: 'Anna', age: 22, gender: 'female' };
-var person_4 = {name: 'Olga', age: 19, gender: 'female' };
-var person_5 = {name: 'Artem', age: 30, gender: 'male' };
-var person_6 = {name: 'Alisa', age: 26, gender: 'female' };
-var person_7 = {name: 'Nikita', age: 28, gender: 'male' };
+var person_1 = {name: 'Nikolay', yearOfBirth: 2000, gender: 'male' };
+var person_2 = {name: 'Alexander', yearOfBirth: 1995, gender: 'male' };
+var person_3 = {name: 'Anna', yearOfBirth: 1997, gender: 'female' };
+var person_4 = {name: 'Olga', yearOfBirth: 1999, gender: 'female' };
+var person_5 = {name: 'Artem', yearOfBirth: 1989, gender: 'male' };
+var person_6 = {name: 'Alisa', yearOfBirth: 1993, gender: 'female' };
+var person_7 = {name: 'Nikita', yearOfBirth: 1992, gender: 'male' };
 var allPeople =[person_1, person_2, person_3, person_4, person_5, person_6, person_7];
 console.log(allPeople);
 
 function checkAverageAge(arr){
     var average = 0;
     for(i = 0; i < arr.length; i++ ){
-        average += arr[i].age;
+        average += 2019 - arr[i].yearOfBirth;
     }
     return average /= arr.length 
 }
@@ -19,23 +19,24 @@ function checkAverageAge(arr){
 function checkAmountGender(arr){
     var male = 0;
     var female = 0;
-    for(i = 0; i < arr.length; i++ ){
-        if(arr[i].gender === 'male'){
+    arr.forEach(function(item) {
+        if (item.gender === 'male') {
             male++;
-        }else{
+        } else {
             female++;
         }
-    }
+    });
     if(male>female){
-        return 'Мужского пола больше'
-    }else if(male<female){
-        return 'Женского пола больше'
+        return 'Мужчин больше'
+    }else if(female>male){
+        return 'Женщин больше'
     }else{
-        return 'Представителей обоих полов равное количество'
+        return 'Равное количество полов'
     }
 }
 
 var averageAge = checkAverageAge(allPeople);
 var amountGender = checkAmountGender(allPeople);
+
 console.log('Средний возраст = ' + averageAge);
 console.log(amountGender);
