adf13617f1c5452dfd227efa01e6d5886336ee59
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-07-23 02:20:14 +0300

    redid

--- task 20/script.js
+++ task 20/script.js
@@ -3,7 +3,7 @@ var divList = document.getElementsByTagName('div');
 function addNumbering(){
     for(var i = 0; i < divList.length ; i++){
         if(i%2){
-            divList[i].innerText = 'Я четный элемент №'+(i/2+0.5);
+            divList[i].innerText = 'Я четный элемент №'+(i/2).toFixed(0);
         }else{
             divList[i].innerText = 'Я нечетный элемент №'+(i/2+1);
         }
