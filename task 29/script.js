422f7e92b613c945bf35d82799c279b85afd36dd
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-08-29 23:39:56 +0300

    redid

--- task 29/script.js
+++ task 29/script.js
@@ -2,16 +2,16 @@ function makeRequst2(url) {
     return fetch(url).then(res => res.json());
 }
 
-let dogsArr;
 makeRequst2('https://dog.ceo/api/breeds/list/all').then(response => {
-    dogsArr = getArrFromObj(response.message);
+  let dogsArr = getArrFromObj(response.message);
     createList(dogsArr);
 })
- 
+
 function getArrFromObj(obj){
     const arr = Object.entries(obj)
     return arr;
 }
+
 
 function createList(dogs) {
 	const fragment = document.createDocumentFragment();
@@ -23,39 +23,18 @@ function createList(dogs) {
 
             clone.innerText = dog[0];
             fragment.appendChild(clone);
-            clickHandler(clone, dog[0])
+            clickHandler(clone, dog[0], dogs)
         });
 
  	const ul = document.getElementsByTagName('ul')[0];
  	ul.appendChild(fragment);
 }
 
-const breedName = document.querySelector('#breedNameTitle');
-const select = document.querySelector('#breeds');
-const images = document.querySelector('#carousel');
-select.onchange = () => {
-    if(select.value){
-        if(images.classList[1]){
-            images.classList.toggle('d-none');
-        };
-        makeRequst2(`https://dog.ceo/api/breed/${breedName.innerText}/${select.value}/images/random/10`)
-        .then(response => generateImage(response.message));
-   ;
-        
-        
-    }
-    else if(!images.classList[1]){
-        images.classList.toggle('d-none');
-    }
-};
-
-const searchBtn = document.querySelector('.searchBtn');
-const searchInput = document.querySelector('.searchInput');
-const container = document.querySelector('.container-1');
-
-function clickHandler(li, name) {
-
-    function breedVerification(event, value = null){
+function clickHandler(li, name, dogsArr) {
+    const select = document.querySelector('#breeds');
+    const images = document.querySelector('#carousel');
+    
+    li.onclick = event =>{
 
         const targetName = event.target.innerText;
 
@@ -67,7 +46,7 @@ function clickHandler(li, name) {
         }
 
         dogsArr.forEach(elem =>{
-            if(targetName === elem[0].toString() || value === elem[0].toString() ){
+            if(targetName === elem[0].toString()){
 
                 if(elem[1].length !== 0){
                     select.innerHTML='<option value="">Выберите разновидность породы</option>';
@@ -91,30 +70,31 @@ function clickHandler(li, name) {
                 }
             }
         })
-        if(value){
-            breedName.innerText = value;
-        }else{
-            breedName.innerText = name;
-        }
+
+        select.onchange = () => {
+            const breedName = document.querySelector('#breedNameTitle');
+            if(select.value){
+                if(images.classList[1]){
+                    images.classList.toggle('d-none');
+                };
+                makeRequst2(`https://dog.ceo/api/breed/${breedName.innerText}/${select.value}/images/random/10`)
+                .then(response => generateImage(response.message)); 
+
+            }
+            else if(!images.classList[1]){
+                images.classList.toggle('d-none');
+            }
+        };
+
+        document.querySelector('#breedNameTitle').innerText = name;
     };
-
-    function inputVerification(e){
-        let value = searchInput.value;
-            dogsArr.forEach(elem =>{
-                if(value === elem[0].toString()){
-                    breedVerification(e, value);
-                }
-            })
-    }
-
-    li.onclick = breedVerification;
-    searchBtn.onclick = inputVerification;
-
 }
 
 
-const ul = document.querySelector('.images');
+
+
 function generateImage(src){
+    const ul = document.querySelector('.images');
     document.querySelectorAll('#images img').forEach(e => e.parentNode.removeChild(e));
   
   
@@ -137,36 +117,55 @@ function generateImage(src){
   ul.appendChild(fragment);
 }
 
-/*SLIDER*/
-/* этот код помечает картинки, для удобства разработки */
-let i = 1;
-for(let li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
+
+document.querySelector('.searchBtn').onclick = () => {
+    const breedUl = document.querySelector('.breedList')
+    const searchValue = document.querySelector('.searchInput').value.toLowerCase();
+    const breedLi = breedUl.getElementsByTagName('li');
+    for(let i = 0; i < breedLi.length; i++){
+        breedLi[i].classList.add('d-none');
+
+        if(breedLi[i].innerText.includes(searchValue)){
+           breedLi[i].classList.toggle('d-none');
+        }
+        
+    }
 }
 
-/* конфигурация */
-let width = 130; // ширина картинки
-let count = 3; // видимое количество изображений
 
-let list = carousel.querySelector('ul');
-let listElems = carousel.querySelectorAll('li');
+/*SLIDER*/
 
-let position = 0; // положение ленты прокрутки
-
-carousel.querySelector('.prev').onclick = function() {
-  // сдвиг влево
-  position += width * count;
-  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-  position = Math.min(position, 0)
-  list.style.marginLeft = position + 'px';
-};
-
-carousel.querySelector('.next').onclick = function() {
-  // сдвиг вправо
-  position -= width * count;
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
-};
+(function(){
+    /* этот код помечает картинки, для удобства разработки */
+    let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
+    
+    /* конфигурация */
+    let width = 130; // ширина картинки
+    let count = 3; // видимое количество изображений
+    
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+    
+    let position = 0; // положение ленты прокрутки
+    
+    carousel.querySelector('.prev').onclick = function() {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+    
+    carousel.querySelector('.next').onclick = function() {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+}());
